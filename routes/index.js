04ff58f741a707ed044487f146b178181f95e132
kyechan99 <kyechan99@naver.com>
2018-08-02 20:46:00 +0900

    [dev] output about data

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  userData = [
+    ['This is temp Title _ 0', 'kyechan', '2018-08-01'],
+    ['This is temp Title _ 1', 'henry', '2018-08-03'],
+    ['This is temp Title _ 2', '0814', '2018-08-03']
+  ];
+  
   res.render('business/index', { title: 'Express' });
 });
 
