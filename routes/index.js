033b3d8b89745de924835edf0735f5b1d6aa9eed
kyechan99 <kyechan99@naver.com>
2018-08-03 18:10:38 +0900

    [dev] navbar function

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next) {  
+  navIdx = 'HOME';
+  res.render('business/index', { title: 'BoardTemplate'});
+});
+router.get('/user', function(req, res, next) {
+  navIdx = 'USER';
   userData = [
     ['This is temp Title _ 0', 'kyechan', '2018-08-01'],
     ['This is temp Title _ 1', 'henry', '2018-08-03'],
     ['This is temp Title _ 2', '0814', '2018-08-03']
   ];
   
-  res.render('business/index', { title: 'Express' });
+  res.render('business/user', { title: 'BoardTemplate-user'});
 });
-
 module.exports = router;
