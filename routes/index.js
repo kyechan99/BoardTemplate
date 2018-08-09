17d808b16ba764b59b13fb3e7e08dbedad559c8d
kyechan99 <kyechan99@naver.com>
2018-08-09 16:04:47 +0900

    [dev] login page & [set] routes modified

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   currentPage = 'HOME';
   res.render('business/index', { title: 'BoardTemplate'});
 });
+/* GET login page. */
+router.get('/login', function(req, res, next) {  
+  currentPage = 'LOGIN';
+  res.render('business/login', { title: 'BoardTemplate-login'});
+});
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET login page. */
-router.get('/login', function(req, res, next) {  
-    currentPage = 'LOGIN';
-    res.render('business/login', { title: 'BoardTemplate-login'});
+router.get('/', function(req, res, next) {  
+    
 });
+
 module.exports = router;
